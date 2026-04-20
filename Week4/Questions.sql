a0bacd8e2fe2a1750d9d578dedf625d42c006214
Bhanu <58395086+LekhanaMitta@users.noreply.github.com>
2026-04-20 18:47:40 -0400

    Week 4 Questions A
    Updated comments for clarity and numbering in SQL queries.

--- Week4/Questions.sql
+++ Week4/Questions.sql
@@ -35,20 +35,20 @@ GROUP BY region_id
 ORDER BY region_id;
 
 -- B. Customer Transactions
--- What is the unique count and total amount for each transaction type?
--- What is the average total historical deposit counts and amounts for all customers?
--- For each month - how many Data Bank customers make more than 1 deposit and either 1 purchase or 1 withdrawal in a single month?
--- What is the closing balance for each customer at the end of the month?
--- What is the percentage of customers who increase their closing balance by more than 5%?
+-- 1. What is the unique count and total amount for each transaction type?
+-- 2. What is the average total historical deposit counts and amounts for all customers?
+-- 3. For each month - how many Data Bank customers make more than 1 deposit and either 1 purchase or 1 withdrawal in a single month?
+-- 4. What is the closing balance for each customer at the end of the month?
+-- 5. What is the percentage of customers who increase their closing balance by more than 5%?
+
 -- C. Data Allocation Challenge
--- To test out a few different hypotheses - the Data Bank team wants to run an experiment where different groups of customers would be allocated data using 3 different options:
+-- 1. To test out a few different hypotheses - the Data Bank team wants to run an experiment where different groups of customers would be allocated data using 3 different options:
+	-- Option 1: data is allocated based off the amount of money at the end of the previous month
+	-- Option 2: data is allocated on the average amount of money kept in the account in the previous 30 days
+	-- Option 3: data is updated real-time
 
--- Option 1: data is allocated based off the amount of money at the end of the previous month
--- Option 2: data is allocated on the average amount of money kept in the account in the previous 30 days
--- Option 3: data is updated real-time
--- For this multi-part challenge question - you have been requested to generate the following data elements to help the Data Bank team estimate how much data will need to be provisioned for each option:
-
--- running customer balance column that includes the impact each transaction
--- customer balance at the end of each month
--- minimum, average and maximum values of the running balance for each customer
+-- 2. For this multi-part challenge question - you have been requested to generate the following data elements to help the Data Bank team estimate how much data will need to be provisioned for each option:
+	-- running customer balance column that includes the impact each transaction
+	-- customer balance at the end of each month
+	-- minimum, average and maximum values of the running balance for each customer
 -- Using all of the data available - how much data would have been required for each option on a monthly basis?
