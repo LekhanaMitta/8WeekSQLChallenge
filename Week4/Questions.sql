4f2e1c8749fb7a93bcf3db45c969478a4b9f7c8c
Bhanu <58395086+LekhanaMitta@users.noreply.github.com>
2026-04-21 20:33:37 -0400

    Week 4 Questions B 1 to 3

--- Week4/Questions.sql
+++ Week4/Questions.sql
@@ -36,8 +36,47 @@ ORDER BY region_id;
 
 -- B. Customer Transactions
 -- 1. What is the unique count and total amount for each transaction type?
+SELECT DISTINCT(txn_type), 
+	COUNT(txn_type),
+    SUM(txn_amount)
+	FROM customer_transactions
+GROUP BY txn_type;
+
 -- 2. What is the average total historical deposit counts and amounts for all customers?
+SELECT AVG(deposit_count) 
+FROM (
+		SELECT txn_date, COUNT(txn_type) AS deposit_count
+            FROM customer_transactions
+        WHERE txn_type = 'deposit'
+        GROUP BY txn_type, txn_date) t;
+
 -- 3. For each month - how many Data Bank customers make more than 1 deposit and either 1 purchase or 1 withdrawal in a single month?
+SELECT month, 
+	COUNT(customer_id)
+	FROM (
+      	SELECT EXTRACT(MONTH FROM txn_date) AS month,
+          customer_id,
+          COUNT(
+              CASE
+                  WHEN txn_type = 'deposit' THEN 1 ELSE 0
+              END) AS deposit_count,
+          COUNT(
+              CASE
+                  WHEN txn_type = 'purchase' THEN 1 ELSE 0
+              END) AS purchase_count,
+          COUNT(
+              CASE
+                  WHEN txn_type = 'withdrawal' THEN 1 ELSE 0
+              END) AS withdrawal_count
+      FROM customer_transactions
+      GROUP BY EXTRACT(MONTH FROM txn_date),
+              customer_id
+      ORDER BY EXTRACT(MONTH FROM txn_date),
+              customer_id) t
+	WHERE deposit_count > 1
+    	AND (purchase_count>1 OR withdrawal_count>1)
+GROUP BY month;
+
 -- 4. What is the closing balance for each customer at the end of the month?
 -- 5. What is the percentage of customers who increase their closing balance by more than 5%?
 
