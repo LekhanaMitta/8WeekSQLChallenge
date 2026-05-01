c48025b543ae8c2542f6efb3af9a3bb3f58a64e2
Bhanu <58395086+LekhanaMitta@users.noreply.github.com>
2026-04-30 23:18:09 -0400

    Week 5 Question A
    Create a clean_weekly_sales table with data cleaning steps including date conversion, week and month extraction, age band and demographic mapping, and average transaction calculation.

--- Week5/Questions.sql
+++ Week5/Questions.sql
@@ -1,3 +1,47 @@
+-- I. DATA CLEANING STEPS
+-- In a single query, perform the following operations and generate a new table in the data_mart schema named clean_weekly_sales:
+
+-- 1. Convert the week_date to a DATE format
+-- 2. Add a week_number as the second column for each week_date value, for example any value from the 1st of January to 7th of January will be 1, 8th to 14th will be 2 etc
+-- 3. Add a month_number with the calendar month for each week_date value as the 3rd column
+-- 4. Add a calendar_year column as the 4th column containing either 2018, 2019 or 2020 values
+-- 5. Add a new column called age_band after the original segment column using the following mapping on the number inside the segment value
+		-- segment	age_band
+		-- 1		Young Adults
+		-- 2		Middle Aged
+		-- 3 or 4	Retirees
+-- 6. Add a new demographic column using the following mapping for the first letter in the segment values:
+		-- segment	demographic
+		-- C		Couples
+		-- F		Families
+-- Ensure all null string values with an "unknown" string value in the original segment column as well as the new age_band and demographic columns
+-- 7. Generate a new avg_transaction column as the sales value divided by transactions rounded to 2 decimal places for each record
+DROP TABLE IF EXISTS clean_weekly_sales;
+CREATE TABLE clean_weekly_sales AS
+SELECT 
+	TO_DATE(week_date, 'DD/MM/YY') AS week_date,
+    EXTRACT(WEEK FROM (TO_DATE(week_date, 'DD/MM/YY'))) AS week_number,
+    EXTRACT(MONTH FROM (TO_DATE(week_date, 'DD/MM/YY'))) AS month,
+    EXTRACT (YEAR FROM (TO_DATE(week_date, 'DD/MM/YY'))) AS year,
+    (CASE 
+      	WHEN segment LIKE '%1' THEN 'Young Adults'
+      	WHEN segment LIKE '%2' THEN 'Middle Aged'
+      	WHEN segment LIKE '%3' THEN 'Retirees'
+     	WHEN segment LIKE '%3' THEN 'Retirees'
+     	ELSE 'Unknown'
+      END) AS age_band,
+     (CASE
+      	WHEN segment LIKE 'C%' THEN 'Couples'
+      	WHEN segment LIKE 'F%' THEN 'Families'
+      	ELSE 'Unknown'
+      END) AS demographic,
+     ROUND((sales/transactions), 2) AS avg_transaction
+FROM weekly_sales;
+
+SELECT * FROM clean_weekly_sales;
+    
+
+-- II. DATE EXPLORATION
 -- 1. What day of the week is used for each week_date value?
 SELECT DISTINCT(EXTRACT(DOW FROM entry_date))
 	FROM (SELECT TO_DATE(week_date, 'DD/MM/YY') 
